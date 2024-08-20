761207700b71b9f811f6f2a528b94c16e1120d2f
Chidiebere Ibiam <chidiebereorjiibiam7@gmail.com>
2024-08-20 09:24:11 +0100

    Added the blog views, templates and url endpoints

--- digitalhub/blog/urls.py
+++ digitalhub/blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path("", views.blog, name="blog"),
+    path('<slug:slug>', views.blog_details, name="blog_details"),
+
 ]
--- digitalhub/blog/views.py
+++ digitalhub/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def blog(request):
+    context = {}
+    return render(request, 'blog/blog.html', context)
+
+
+def blog_details(request, slug):
+    context = {}
+    return render(request, 'blog/blog-details.html', context)
