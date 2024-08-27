c493d6b6f1b84f7178499002b218183873ce44cd
Chidiebere Ibiam <chidiebereorjiibiam7@gmail.com>
2024-08-27 15:56:16 +0100

    "Updated ALLOWED_HOSTS in settings.py from a list of specific hosts to a wildcard (*)"

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://digitalhub-indol.vercel.app/", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
