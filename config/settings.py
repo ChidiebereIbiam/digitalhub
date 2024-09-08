a1ec2c4eaea2abac88254acebc1b967a30230055
Chidiebere Ibiam <chidiebereorjiibiam7@gmail.com>
2024-09-08 21:30:26 +0100

    Updated Pricing Page and DB Configuration in Settings.py

--- config/settings.py
+++ config/settings.py
@@ -120,11 +120,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST':os.environ.get('DB_HOST'),
-        'PORT':os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST':os.environ.get('MYSQL_HOST'),
+        'PORT':os.environ.get('MYSQL_PORT'),
         'OPTIONS': {
             'unix_socket': '/opt/lampp/var/mysql/mysql.sock',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
