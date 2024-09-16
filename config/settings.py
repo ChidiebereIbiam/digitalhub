c2b86aae23e2b78436db7e4dc72ce03290f33532
Chidiebere Ibiam <chidiebereorjiibiam7@gmail.com>
2024-09-17 00:38:47 +0100

    "Updated .gitignore to include /temp directory and modified database settings in config/settings.py"

--- config/settings.py
+++ config/settings.py
@@ -117,6 +117,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#LOCAL
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get('MYSQL_DATABASE'),
+#         'USER': os.environ.get('MYSQL_USER'),
+#         'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+#         'HOST':os.environ.get('MYSQL_HOST'),
+#         'PORT':os.environ.get('MYSQL_PORT'),
+#         'OPTIONS': {
+#             'unix_socket': '/opt/lampp/var/mysql/mysql.sock',
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -126,7 +141,6 @@ DATABASES = {
         'HOST':os.environ.get('MYSQL_HOST'),
         'PORT':os.environ.get('MYSQL_PORT'),
         'OPTIONS': {
-            'unix_socket': '/opt/lampp/var/mysql/mysql.sock',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
