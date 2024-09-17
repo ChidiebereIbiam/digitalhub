c3990759cd3609b01bd51a63c8d49233025c4269
Chidiebere Ibiam <chidiebereorjiibiam7@gmail.com>
2024-09-17 14:13:09 +0100

    Added allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["247digitalhub.com", "localhost"]
+CSRF_TRUSTED_ORIGINS = ['https://247digitalhub.com/', 'https://247digitalhub.com']
 
 
 # Application definition
@@ -118,20 +119,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 #LOCAL
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE'),
-        'USER': os.environ.get('MYSQL_USER'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
-        'HOST':os.environ.get('MYSQL_HOST'),
-        'PORT':os.environ.get('MYSQL_PORT'),
-        'OPTIONS': {
-            'unix_socket': '/opt/lampp/var/mysql/mysql.sock',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
-}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -141,10 +128,24 @@ DATABASES = {
 #         'HOST':os.environ.get('MYSQL_HOST'),
 #         'PORT':os.environ.get('MYSQL_PORT'),
 #         'OPTIONS': {
+#             'unix_socket': '/opt/lampp/var/mysql/mysql.sock',
 #             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
 #         },
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST':os.environ.get('MYSQL_HOST'),
+        'PORT':os.environ.get('MYSQL_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
 
 AUTH_USER_MODEL = 'authentication.User'
 # Password validation
